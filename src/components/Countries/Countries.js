ec9fe0eb9801c16651dac11e3c60fc9e42426f70
rakibsarowar <rakibsarowar@gmail.com>
2023-03-24 22:55:47 +0600

    flag added & used some styles

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = () => {
     // Step 01: Use state -------------------------------------------------
@@ -16,19 +17,22 @@ const Countries = () => {
     }, [])
 
     return (
-        <div>
+        <div >
             <h1>Total Countries: {countries.length}</h1>
-            {   // Step 05: Showing the data in UI ------------------------
+            <div className='countries-container'>
+                {   // Step 05: Showing the data in UI ------------------------
 
-                /* 
-                checking the data from map -------------------
-                countries.map(country=>console.log(country)) 
-                */
-                countries.map(country => <Country
-                    country = {country}>
-                </Country>)
+                    /* 
+                    checking the data from map -------------------
+                    countries.map(country=>console.log(country)) 
+                    */
+                    countries.map(country => <Country
+                        country={country}
+                        key = {country.cca3}
+                    ></Country>)
 
-            }
+                }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -3,11 +3,17 @@ import './Country.css'
 
 const Country = (props) => {
     // console.log(props)
+
+    // array destructuring ----------------------------------
+    const {region,area, population,name,flags} = props.country;
+
     return (
         <div className='country'>
-            <h2>Country Name:{props.country.name.common} </h2>
-            <p>Population:{props.country.population} </p>
-            <p><small>Area: {props.country.area}</small></p>
+            <h2>Country Name:{name.common} </h2>
+            <img src={flags.png} alt="" />
+            <p>Population:{population} </p>
+            <p><small>Area: {area}</small></p>
+            <p><small>Region: {region}</small></p>
         </div>
     );
 };
